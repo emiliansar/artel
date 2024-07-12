6a0d2e36311d080f4abc3337a1bec4143bf9639c
Emil <emiliansar@gmail.com>
2024-07-12 14:11:15 +0400

    base first page successfully created

--- js/slick--slider.js
+++ js/slick--slider.js
@@ -41,4 +41,50 @@ $(document).ready(function () {
 	$(".o--w__slider").on('afterChange', function (event, slick, currentSlide) {
 		$(".current--slide").text(`${currentSlide + 1}`)
 	});
+	$(".worker--text__slider").slick({
+		arrows: false,
+		dots: false,
+		slidesToShow: 1,
+		infinite: false,
+		waitForAnimate: false,
+		vertical: true,
+		verticalSwiping: true,
+		autoplaySpeed: 100000,
+		asNavFor: ".worker--images__slider",
+	});
+	$(".worker--text__slider").on('wheel', function (e) {
+		e.preventDefault();
+		if (e.originalEvent.deltaY > 0) {
+			$(this).slick('slickNext');
+			console.log(e.originalEvent.deltaY);
+		} else {
+			$(this).slick('slickPrev');
+			console.log(e.originalEvent.deltaY);
+		}
+	});
+	$(".worker--images__slider").slick({
+		arrows: false,
+		dots: false,
+		slidesToShow: 1,
+		infinite: false,
+		waitForAnimate: false,
+		vertical: true,
+		verticalSpacing: 32,
+		verticalSwiping: true,
+		autoplaySpeed: 100000,
+		asNavFor: ".worker--text__slider",
+	});
+	$(".worker--images__slider").on('wheel', function (e) {
+		e.preventDefault();
+		if (e.originalEvent.deltaY > 0) {
+			$(this).slick('slickNext');
+			console.log(e.originalEvent.deltaY);
+		} else {
+			$(this).slick('slickPrev');
+			console.log(e.originalEvent.deltaY);
+		}
+	});
+	$(".worker--images__slider").on("afterChange", function (event, slick, currentSlide) {
+		$(".worker--counter__current").text(`${currentSlide + 1} `);
+	});
 });
---
+++ js/swiper--slider.js
@@ -0,0 +1,11 @@
+var textSlider = new Swiper(".worker--text__slider", {
+	direction: "vertical",
+	slidesPerView: 1,
+	spaceBetween: 32,
+});
+
+var imagesSlider = new Swiper(".worker--images__slider", {
+	direction: "vertical",
+	slidesPerView: 1,
+	spaceBetween: 32,
+});
